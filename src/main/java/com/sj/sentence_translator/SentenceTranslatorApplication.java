7ad41ee699d7629e9a387e645bfc9a45d038ad19
Szymon Jedrzejewski <szymonj219@gmail.com>
2021-06-12 23:07:13 +0200

    Initial commit

---
+++ src/main/java/com/sj/sentence_translator/SentenceTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.sj.sentence_translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentenceTranslatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentenceTranslatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sj/sentence_translator/SentenceTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sj.sentence_translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentenceTranslatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
